5ced6c5bdd22a594985d81d5f38e8e9a4a5e78c8
EvanRau <evanrau@hawaii,edu>
2023-02-23 09:34:17 -0800

    Initial implementation of epicCats

---
+++ src/Legacy/Cat.h
@@ -0,0 +1,13 @@
+///////////////////////////////////////////////////////////////////////////////
+//         University of Hawaii, College of Engineering
+//         Animal Farm - EE 205 - Spr 2023
+//
+///
+/// @file    Cat.h
+/// @author  Evan Rau <evanrau@hawaii.edu>
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef EE205_ANIMAL_FARM_CAT_H
+#define EE205_ANIMAL_FARM_CAT_H
+
+#endif //EE205_ANIMAL_FARM_CAT_H
---
+++ src/Legacy/epicCats.cpp
@@ -0,0 +1,13 @@
+///////////////////////////////////////////////////////////////////////////////
+//         University of Hawaii, College of Engineering
+//         Animal Farm - EE 205 - Spr 2023
+//
+///
+/// @file    epicCats.cpp
+/// @author  Evan Rau <evanrau@hawaii.edu>
+///////////////////////////////////////////////////////////////////////////////
+#include <stdio.h>
+int main(){
+    printf("Hello World");
+    return 0;
+}
