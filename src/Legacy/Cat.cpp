b195ba6165566590793b9aae824dee74b20265a1
EvanRau <evanrau@hawaii,edu>
2023-02-23 10:17:15 -0800

    Implemented validateCat ; Defined list of Cat Names ; Initial implementation of generateCat

--- src/Legacy/Cat.cpp
+++ src/Legacy/Cat.cpp
@@ -7,3 +7,28 @@
 /// @author  Evan Rau <evanrau@hawaii.edu>
 ///////////////////////////////////////////////////////////////////////////////
 #include "Cat.h"
+
+
+bool validateCat(Cat Cat){
+    if (sizeof(Cat.Name) <= 0 || sizeof(Cat.Name) > MAX_CAT_NAME){ //Invalid Name
+        return false;
+    }
+    if (Cat.Gender != MALE && Cat.Gender != FEMALE && Cat.Gender != UNKNOWN){ //Invalid Gender
+        return false;
+    }
+
+    if (Cat.weightInPounds <= 0 || Cat.weightInPounds >= 100){ //Invalid weightInPounds
+        return false;
+    }
+    if (sizeof(Cat.chipID) == 0){ //Invalid chipID
+        return false;
+    }
+    if (Cat.isFixed != false && Cat.isFixed != true){ //Invalid isFixed
+        return false;
+    }
+    return true; //Valid Cat
+}
+
+struct Cat generateCat(){
+
+}
