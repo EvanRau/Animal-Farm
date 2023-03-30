74e34627cf06baf5895721bbd00db4f92bf603c3
EvanRau <evanrau@hawaii.edu>
2023-03-29 21:05:16 -1000

    began tests for gender class

---
+++ tests/test_Gender.cpp
@@ -0,0 +1,28 @@
+///////////////////////////////////////////////////////////////////////////////
+//         University of Hawaii, College of Engineering
+//         Animal Farm - EE 205 - Spr 2023
+//
+/// @file    test_Legacy_epicCats.cpp
+/// @author  Evan Rau <evanrau@hawaii.edu>
+//////////////////////////////////////////////////////////////////////////////
+
+#define BOOST_TEST_DYN_LINK
+
+#include <boost/test/tools/output_test_stream.hpp>
+#include <boost/test/unit_test.hpp>
+
+#include "../src/Utility/Gender.h"
+
+
+using namespace std;
+
+BOOST_AUTO_TEST_SUITE( test_Gender )
+
+    BOOST_AUTO_TEST_CASE( test_validateGender ) {
+        Gender testGender = newRandomGender();   BOOST_CHECK_EQUAL(validateGender(testGender), true);
+        testGender = static_cast<Gender>(0);    BOOST_CHECK_EQUAL(validateGender(testGender), true);
+        testGender = static_cast<Gender>(1);    BOOST_CHECK_EQUAL(validateGender(testGender), true);
+        testGender = static_cast<Gender>(2);    BOOST_CHECK_EQUAL(validateGender(testGender), true);
+        testGender = static_cast<Gender>(10);    BOOST_CHECK_EQUAL(validateGender(testGender), false);
+    }
+}
