8c40e0607aeed421ae9f0f2ca84640681732a683
EvanRau <evanrau@hawaii.edu>
2023-03-29 21:12:22 -1000

    Created Animal Directory and Class

---
+++ src/Animal/Animal.h
@@ -0,0 +1,19 @@
+///////////////////////////////////////////////////////////////////////////////
+//         University of Hawaii, College of Engineering
+//         Animal Farm - EE 205 - Spr 2023
+//
+///
+/// @file    Animal.h
+/// @author  Evan Rau <evanrau@hawaii.edu>
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef EE205_ANIMAL_FARM_ANIMAL_H
+#define EE205_ANIMAL_FARM_ANIMAL_H
+
+
+class Animal {
+
+};
+
+
+#endif //EE205_ANIMAL_FARM_ANIMAL_H
