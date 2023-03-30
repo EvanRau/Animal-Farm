b6e887d842b9e233b0b9a7593b6004e361e64cdf
EvanRau <evanrau@hawaii.edu>
2023-03-29 21:51:45 -1000

    Created Animal Directory and Class

--- src/Animal/Animal.h
+++ src/Animal/Animal.h
@@ -11,10 +11,31 @@
 #define EE205_ANIMAL_FARM_ANIMAL_H
 
 
+#include "../Utility/Gender.h"
+#include <string>
 
+///Properties of an animal. Includes its gender and its kingdom
 class Animal {
-
+    public:
+        ///Gender of animal; Unknown by default
+        Gender gender = Gender::UNKNOWN_GENDER;
+        ///Kingdom of animal (always Animalia)
+        std::string_view Kingdom = "Animalia";
+        ///Info of an animal; "An animal" by default
+        std::string info = "An animal";
 };
+
+extern std::string_view getKingdom(Animal animal);
+
+extern Gender getGender(Animal animal);
+
+extern void setGender(Animal animal);
+
+extern bool validate(Animal animal);
+
+extern std::string info(Animal animal);
+
+extern void dump(Animal animal);
 
 
 #endif //EE205_ANIMAL_FARM_ANIMAL_H
