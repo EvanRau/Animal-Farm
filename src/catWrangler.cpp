bb38279dbffb9f0edc95bf1a2b7412d8bad85703
EvanRau <evanrau@hawaii.edu>
2023-04-12 22:42:35 -1000

    Created catWrangler.cpp

---
+++ src/catWrangler.cpp
@@ -0,0 +1,16 @@
+///////////////////////////////////////////////////////////////////////////////
+//         University of Hawaii, College of Engineering
+//         Animal Farm - EE 205 - Spr 2023
+//
+///
+/// @file    catWrangler.cpp
+/// @author  Evan Rau <evanrau@hawaii.edu>
+///////////////////////////////////////////////////////////////////////////////
+
+
+
+#include <iostream>
+
+int main(){
+    std::cout<<"catWrangler"<<std::endl;
+}
